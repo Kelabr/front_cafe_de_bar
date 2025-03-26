3bcecb22d8556e7901300af0f9a552a85b6492d1
Kel_Seduc <kelabrantes.me@gmail.com>
2025-03-26 15:57:41 -0300

    Incluindo placeholder

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import { Search } from "lucide-react"
 export default function SearchBar(){
     return(
         <form className="flex w-full border-2 rounded-lg p-1 mt-2">
-            <input type="text" className=" w-full outline-0 pl-1"/>
+            <input type="text" className=" w-full outline-0 pl-1" placeholder="Pesquise pela cidade desejada"/>
             <button type="submit"><Search color="#bfbfbf"/></button>
         </form>
     )
