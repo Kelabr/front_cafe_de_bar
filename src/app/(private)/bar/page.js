09e66ffabd53fd854bfede1d3e01526b6cc5ac84
Kel_Seduc <kelabrantes.me@gmail.com>
2025-03-26 15:16:14 -0300

    começando a fazer a página bar, criando componente SearchBar para efetuar a pesquisa

--- src/app/(private)/bar/page.js
+++ src/app/(private)/bar/page.js
@@ -1,7 +1,13 @@
+import Header from "@/components/Header";
+import SearchBar from "@/components/SearchBar";
+
 export default function Bar(){
     return(
-        <main>
-            
+        <main className="h-screen">
+                <Header/>
+                <div className="px-10">
+                    <SearchBar/>
+                </div>
         </main>
     )
 }
