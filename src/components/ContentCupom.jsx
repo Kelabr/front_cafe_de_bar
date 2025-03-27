68cd06c3795c26b06e0966d3aa12f5890f533d86
morphis-code <morphis.code@gmail.com>
2025-03-27 11:22:02 -0300

    integrando dados do banco de dados com a tela de conteudo dos cupons

--- src/components/ContentCupom.jsx
+++ src/components/ContentCupom.jsx
@@ -21,6 +21,7 @@ export default function ContentCupom(){
                 const response = await fetch(`http://localhost:3333/api/cupons/${tipoCupons}/${id}`)
                 const data = await response.json()
                 setCupom(data)
+                console.log(data)
             }catch(error){
                 console.error("Erro ao buscar cupom")
             }
@@ -29,10 +30,14 @@ export default function ContentCupom(){
         fetchContentCupom()
     },[])
 
+    if (!cupom.length) {
+        return <div>Carregando...</div> // Mostra um loading enquanto os dados não chegam
+    }
+
     return(
         <div>
-            <h1 className="text-white font-bold text-5xl">15% Toa a Tôa</h1>
-            <p className="text-white text-lg pl-2">Melhor restaurante do canal 7</p>
+            <h1 className="text-white font-bold text-5xl">{cupom[0].porcentagem}% {cupom[0].nome}</h1>
+            <p className="text-white text-lg pl-2">{cupom[0].descricao}</p>
             <div className="flex gap-2 mt-6">
                 <MapPin color="white"/>
                 <p className="text-white">Rua bla bla, 555, blabla - Santos</p>
